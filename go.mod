c32baae22ef47135b61cfd3ac703d424ed036101
Gabe Cook <gabe565@gmail.com>
2023-08-28 18:16:48 -0500

    feat: Add man page generator

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/r3labs/diff/v3 v3.0.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.7.0
+	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.16.0
 )
 
@@ -36,7 +37,6 @@ require (
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
---
+++ internal/generate/manpages/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"compress/gzip"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/gabe565/domain-watch/cmd"
+	"github.com/spf13/cobra/doc"
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	flags := flag.NewFlagSet("", flag.ContinueOnError)
+
+	var version string
+	flags.StringVar(&version, "version", "beta", "Version")
+
+	var dateParam string
+	flags.StringVar(&dateParam, "date", time.Now().Format(time.RFC3339), "Build date")
+
+	if err := flags.Parse(os.Args); err != nil {
+		panic(err)
+	}
+
+	if err := os.RemoveAll("manpages"); err != nil {
+		panic(err)
+	}
+
+	if err := os.MkdirAll("manpages", 0o755); err != nil {
+		panic(err)
+	}
+
+	rootCmd := cmd.NewCommand()
+	name := rootCmd.Name()
+
+	date, err := time.Parse(time.RFC3339, dateParam)
+	if err != nil {
+		panic(err)
+	}
+
+	header := doc.GenManHeader{
+		Title:   strings.ToUpper(name),
+		Section: "1",
+		Date:    &date,
+		Source:  name + " " + version,
+		Manual:  "User Commands",
+	}
+
+	f, err := os.Create(filepath.Join("manpages", name+".1.gz"))
+	if err != nil {
+		panic(err)
+	}
+	gz := gzip.NewWriter(f)
+
+	if err := doc.GenMan(rootCmd, &header, gz); err != nil {
+		panic(err)
+	}
+
+	if err := gz.Close(); err != nil {
+		panic(err)
+	}
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+}
