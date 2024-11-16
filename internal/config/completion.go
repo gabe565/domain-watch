fabc66df752023fa8c75f5adf751bdb4120adf15
Gabe Cook <gabe565@gmail.com>
2024-11-15 18:14:33 -0600

    chore: Clean up flag error handling with must library

--- internal/config/completion.go
+++ internal/config/completion.go
@@ -1,47 +1,43 @@
 package config
 
 import (
-	"errors"
 	"log/slog"
 	"strings"
 
+	"gabe565.com/utils/must"
 	"github.com/spf13/cobra"
 )
 
 func RegisterCompletions(cmd *cobra.Command) {
-	if err := errors.Join(
-		cmd.RegisterFlagCompletionFunc(FlagCompletion, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-			return []string{"bash", "zsh", "fish", "powershell"}, cobra.ShellCompDirectiveNoFileComp
-		}),
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagCompletion, func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return []string{"bash", "zsh", "fish", "powershell"}, cobra.ShellCompDirectiveNoFileComp
+	}))
 
-		cmd.RegisterFlagCompletionFunc(FlagDomains, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagEvery, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagSleep, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagThreshold, cobra.NoFileCompletions),
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagDomains, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagEvery, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagSleep, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagThreshold, cobra.NoFileCompletions))
 
-		cmd.RegisterFlagCompletionFunc(FlagMetricsEnabled, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagMetricsAddress, cobra.NoFileCompletions),
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagMetricsEnabled, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagMetricsAddress, cobra.NoFileCompletions))
 
-		cmd.RegisterFlagCompletionFunc(FlagLogLevel,
-			func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-				return []string{
-					strings.ToLower(slog.LevelDebug.String()),
-					strings.ToLower(slog.LevelInfo.String()),
-					strings.ToLower(slog.LevelWarn.String()),
-					strings.ToLower(slog.LevelError.String()),
-				}, cobra.ShellCompDirectiveNoFileComp
-			}),
-		cmd.RegisterFlagCompletionFunc(FlagLogFormat,
-			func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-				return []string{"text", "json"}, cobra.ShellCompDirectiveNoFileComp
-			}),
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagLogLevel,
+		func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+			return []string{
+				strings.ToLower(slog.LevelDebug.String()),
+				strings.ToLower(slog.LevelInfo.String()),
+				strings.ToLower(slog.LevelWarn.String()),
+				strings.ToLower(slog.LevelError.String()),
+			}, cobra.ShellCompDirectiveNoFileComp
+		}))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagLogFormat,
+		func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+			return []string{"text", "json"}, cobra.ShellCompDirectiveNoFileComp
+		}))
 
-		cmd.RegisterFlagCompletionFunc(FlagTelegramToken, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagTelegramChat, cobra.NoFileCompletions),
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTelegramToken, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagTelegramChat, cobra.NoFileCompletions))
 
-		cmd.RegisterFlagCompletionFunc(FlagGotifyURL, cobra.NoFileCompletions),
-		cmd.RegisterFlagCompletionFunc(FlagGotifyToken, cobra.NoFileCompletions),
-	); err != nil {
-		panic(err)
-	}
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagGotifyURL, cobra.NoFileCompletions))
+	must.Must(cmd.RegisterFlagCompletionFunc(FlagGotifyToken, cobra.NoFileCompletions))
 }
